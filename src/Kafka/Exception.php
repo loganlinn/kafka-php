d6853b1897f324390dd8a8c3286184612b08d5fa
Ahmy <ahmy@sense-os.nl>
2013-11-15 14:02:55 +0000

    Create more exception class following Kafka broker response error

--- src/Kafka/Exception.php
+++ src/Kafka/Exception.php
@@ -4,6 +4,49 @@ namespace Kafka;
 
 class Exception extends \RuntimeException
 {
+    /**
+     * Create Response exception from kafka server
+     */
+    public static function createException(
+        $errorNo,
+        $message = null,
+        $code = 0,
+        Excpetion $previous = null
+    )
+    {
+        switch($errorNo) {
+        case -1:
+            $errorClass = \Kafka\Unknown;
+        case 1:
+            $errorClass = \Kafka\OffsetOutOfRange;
+        case 2:
+            $errorClass = \Kafka\InvalidMessage;
+        case 3:
+            $errorClass = \Kafka\UnknownTopicOrPartition;
+        case 4:
+            $errorClass = \Kafka\InvalidMessageSize;
+        case 5:
+            $errorClass = \Kafka\LeaderNotAvailable;
+        case 6:
+            $errorClass = \Kafka\NotLeaderForPartition;
+        case 7:
+            $errorClass = \Kafka\RequestTimedOut;
+        case 8:
+            $errorClass = \Kafka\BrokerNotAvailable;
+        case 9:
+            $errorClass = \Kafka\ReplicaNotAvailable;
+        case 10:
+            $errorClass = \Kafka\MessageSizeTooLarge;
+        case 11:
+            $errorClass = \Kafka\StaleControllerEpochCode;
+        case 12:
+            $errorClass = \Kafka\OffsetMetadataTooLargeCode;
+        default:
+            return new self("Unknwon response error code number exception");
+        }
+
+        return new $errorClass($message, $code, $previous);
+    }
 }
 
 namespace Kafka\Exception;
@@ -13,5 +56,47 @@ class EndOfStream extends \Kafka\Exception
 }
 
 class TopicUnavailable extends \Kafka\Exception
+{
+}
+
+class Unknown extends \Kafka\Exception
+{
+}
+
+class OffsetOutOfRange extends \Kafka\Exception
+{
+}
+
+class InvalidMessage extends \Kafka\Exception
+{
+}
+class UnknownTopicOrPartition extends \Kafka\Exception
+{
+}
+class InvalidMessageSize extends \Kafka\Exception
+{
+}
+class LeaderNotAvailable extends \Kafka\Exception
+{
+}
+class NotLeaderForPartition extends \Kafka\Exception
+{
+}
+class RequestTimedOut extends \Kafka\Exception
+{
+}
+class BrokerNotAvailable extends \Kafka\Exception
+{
+}
+class ReplicaNotAvailable extends \Kafka\Exception
+{
+}
+class MessageSizeTooLarge extends \Kafka\Exception
+{
+}
+class StaleControllerEpochCode extends \Kafka\Exception
+{
+}
+class OffsetMetadataTooLargeCode extends \Kafka\Exception
 {
 }
