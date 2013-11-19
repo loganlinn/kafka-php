629d10e8ae7b704a5a0130653dc76bf6a3db331b
Ahmy <ahmy@sense-os.nl>
2013-11-19 13:19:56 +0000

    Fix filter test
    previous test case expect blacklist with '.*' return empty array.
    but the implementation showed that it should throw exception.

--- tests/Kafka/TopicFilterTest.php
+++ tests/Kafka/TopicFilterTest.php
@@ -24,8 +24,6 @@ assert($filter->getTopics($topics) === array('topic1','topic2','topictest'));
 $filter = new \Kafka\Whitelist("topic.*");
 assert($filter->getTopics($topics) === array('topic','topic1','topic2','topictest'));
 
-$filter = new \Kafka\Blacklist(".*");
-assert($filter->getTopics($topics) === array());
 
 $filter = new \Kafka\Blacklist("topic.");
 assert($filter->getTopics($topics) === array('topic','topictest','useractivity','pageviews','datasync'));
@@ -35,3 +33,15 @@ assert($filter->getTopics($topics) === array('topic','useractivity','pageviews',
 
 $filter = new \Kafka\Blacklist("topic.*");
 assert($filter->getTopics($topics) === array('useractivity','pageviews','datasync'));
+
+try {
+    $filter = new \Kafka\Blacklist(".*");
+    assert($filter->getTopics($topics) === array()); //should trhow exceptions
+
+    throw new Exception("testcase failed"); // if it reaches this line something is wrong
+} catch(Exception $e) {
+    $message = $e->getMessage();
+    if ($e == "testcase failed") {
+        throw $e;
+    }
+}
